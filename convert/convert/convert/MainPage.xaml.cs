7bfb5020f70fed301dc5bbf2bc3ef4fa94eaa072
ch palomequer <christianpalomeque1989@gmail.com>
2017-08-14 22:45:35 -0500

    task

--- convert/convert/convert/MainPage.xaml.cs
+++ convert/convert/convert/MainPage.xaml.cs
@@ -12,6 +12,32 @@ namespace convert
         public MainPage()
         {
             InitializeComponent();
+
+            ConvertButton.Clicked += ConvertButton_Clicked; 
+        }
+
+        private void ConvertButton_Clicked(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(ConvertEntry.Text))
+            {
+                DisplayAlert("Error", "You must enter a value in celsius. ", "Accept");
+                return;
+            }
+
+            decimal celsius = 0;
+            if (decimal.TryParse(ConvertEntry.Text, out celsius))
+            {
+
+
+                decimal a = 1.8M;
+                decimal b = 32;
+
+                var fahrenheit = celsius * a + b;
+
+                FahrenheitEntry.Text = string.Format("{0:N}", fahrenheit);
+
+            }
+            else { DisplayAlert("Error", "You must enter a value numeric in celsius. ", "Accept"); }
         }
     }
 }
